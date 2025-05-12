cd5074ee20532440281024369355b931bd982b99
Giuseppe Maglione <114797423+Peppe1604@users.noreply.github.com>
2025-05-12 20:40:07 +0200

    Aggiunta del Main

---
+++ Main.java
@@ -0,0 +1,29 @@
+import core.GameStartManagement;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("CIAO BENVENUTO NEL GIOCO TRIS");
+        System.out.println("COME VUOI IMPOSTARE LA PARTITA?");
+        System.out.println("1. Giocatore vs Giocatore");
+        System.out.println("2. Giocatore vs Computer");
+        System.out.println("3. Esci dal gioco");
+        System.out.println("Scegli un opzione: ");
+        int scelta = Integer.parseInt(System.console().readLine());
+        switch (scelta) {
+            case 1:
+                GameStartManagement GiocatoreVsGiocatore = new GameStartManagement();
+                GiocatoreVsGiocatore.GiocatoreVsGiocatore(args);
+                break;
+            case 2:
+                GameStartManagement GiocatoreVsComputer = new GameStartManagement();
+                GiocatoreVsComputer.GiocatoreVsComputer(args);
+                break;
+            case 3:
+                break;
+            default:
+                System.out.println("Scelta non valida. Riprova.");
+        }
+
+    }
+
+}
